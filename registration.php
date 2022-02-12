b75cda79ab1feabdab0e78458d4dd487c92f39f5
mbangse marcel <mbangsemarcel@gmail.com>
2022-02-12 08:52:30 +0100

    The sample registration form for library system still to be updated and the database

---
+++ registration.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+</head>
+<body>
+<form action="adduser.php" method="post" border="4%">
+    <center><table border="0" cellspacing="1" cellpadding="3">
+    <tr><td colspan="6" align="center" ><h3>LIBRARY REGISTRATION FORM</h3></td>
+    </tr>
+    <tr><td>BOOK_ID: </td><td><input size="20" type="text"
+    name="BOOK_ID"></td></tr>
+    <tr><td>Book Name </td><td><input size="50" type="text"
+    name="Book_Name "></td></tr>
+    <tr><td>Author: </td><td><input size="20" type="text"
+    name="Author"></td></tr>
+    <tr><td>Published date: </td><td><input size="30" type="text"
+    name="Published date"></td></tr>
+  <tr><td><input type="submit" name="submit" value="Submit" color="blue"></td><td>
+    <input type="reset" value="Cancel"></td></tr>
+    </table></center>
+</form>
+</body>
+</html>
